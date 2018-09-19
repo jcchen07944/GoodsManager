04e75a8b5ee55d8eeb09a00766169dbe4bead5e2
JCChen <james35022000@gmail.com>
2018-09-20 01:27:07 +0800

    Adjust color and some view.

--- app/src/main/java/jcchen/goodsmanager/view/MainActivity.java
+++ app/src/main/java/jcchen/goodsmanager/view/MainActivity.java
@@ -1,15 +1,37 @@
 package jcchen.goodsmanager.view;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import jcchen.goodsmanager.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private TextView toolbar_title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+        toolbar_title = (TextView) findViewById(R.id.toolbar_title);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+        toolbar_title.setText(getResources().getString(R.string.purchase));
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+
 }
