5e2efd7835d58fef43d6eaa7af3d440d44b81993
JCChen <james35022000@gmail.com>
2018-10-15 00:24:47 +0800

    Size detail dialog fragment.

---
+++ app/src/main/java/jcchen/goodsmanager/view/fragment/SizeDetailDialogFragment.java
@@ -0,0 +1,18 @@
+package jcchen.goodsmanager.view.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import jcchen.goodsmanager.R;
+
+public class SizeDetailDialogFragment extends DialogFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout., container);
+        return view;
+    }
+}
