6b401e2ad159ce7915b51717addeee36a6a15f72
JCChen <james35022000@gmail.com>
2018-11-29 05:22:31 +0800

    Try to solve conflict.

--- app/src/main/java/jcchen/goodsmanager/presenter/impl/PurchasePresenterImpl.java
+++ app/src/main/java/jcchen/goodsmanager/presenter/impl/PurchasePresenterImpl.java
@@ -42,7 +42,7 @@ public class PurchasePresenterImpl implements PurchasePresenter {
             }
         }
 
-        return purchaseList;
+        return sortPurchaseList(purchaseList);
     }
 
     @Override
